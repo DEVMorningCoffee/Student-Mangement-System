69dff353d45e52784203c41b705d3ac6eb766856
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-03-29 21:18:54 -0400

    Can add student to this program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,51 @@
 package org.example;
 
+import org.example.Student.Student;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Student student;
+
+        while(true){
+            String[] choices = {"1: Add Student","2: Get Student"};
+            for(String choice: choices){
+                System.out.println(choice);
+            }
+
+            // Get User input
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Please enter your choice: ");
+
+            int userChoice = Integer.parseInt(scanner.nextLine());
+
+            switch(userChoice){
+                case 1:
+                    String firstName = userInput("What's the firstname: ");
+                    String lastName = userInput("What's the lastname: ");
+                    double balance = Double.parseDouble(userInput("What's the balance: "));
+                    int age = Integer.parseInt(userInput("What's the age: "));
+
+                    student = new Student(firstName, lastName, age, balance);
+
+                    System.out.println(student.toString());
+
+                    break;
+                case 2:
+
+            }
+
+        }
+
+    }
+
+    public static String userInput(String question){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(question);
+        return scanner.nextLine();
     }
 }
