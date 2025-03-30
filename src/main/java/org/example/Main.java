b5c7383f2d449b679b3110592eec7e97b392c5cf
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-03-30 12:38:20 -0400

    Created the userchoice methods and add a function to update user information

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.example.Student.Student;
 
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -11,11 +13,17 @@ public class Main {
 
         Student student;
 
+        ArrayList<Student> students = new ArrayList<>();
+
+        Optional<Student> findStudent;
+        String uuid;
+
         while(true){
-            String[] choices = {"1: Add Student","2: Get Student"};
+            String[] choices = {"1: Add Student","2: Get Student", "3: Update Student"};
             for(String choice: choices){
                 System.out.println(choice);
             }
+
 
             // Get User input
             Scanner scanner = new Scanner(System.in);
@@ -32,15 +40,56 @@ public class Main {
 
                     student = new Student(firstName, lastName, age, balance);
 
-                    System.out.println(student.toString());
+                    students.add(student);
+
+                    System.out.println(student);
 
                     break;
                 case 2:
+                    uuid = userInput("What's the uuid: ");
 
+                    findStudent = findUserChoice(uuid, students);
+
+                    if(findStudent.isPresent()){
+                        System.out.println(findStudent);
+                    }
+
+                    break;
+                case 3:
+                    uuid = userInput("What's the uuid: ");
+                    findStudent = findUserChoice(uuid, students);
+
+                    if(findStudent.isEmpty()){
+                        break;
+                    }
+
+                    String updateFirstName = userInput("What's the firstname: ");
+                    String UpdateLastName = userInput("What's the lastname: ");
+                    double updateBalance = Double.parseDouble(userInput("What's the balance: "));
+                    int updateAge = Integer.parseInt(userInput("What's the age: "));
+
+                    findStudent.get().setFirstName(updateFirstName);
+                    findStudent.get().setLastName(UpdateLastName);
+                    findStudent.get().setBalance(updateBalance);
+                    findStudent.get().setAge(updateAge);
+
+                    break;
             }
 
         }
 
+    }
+
+    public static Optional<Student> findUserChoice(String uuid, ArrayList<Student> students){
+        Optional<Student> optionalStudent = students.stream().filter(s -> s.getUuid().equals(uuid)).findFirst();
+
+        if(optionalStudent.isPresent()){
+            return optionalStudent;
+        }else{
+            System.out.println("Student not found");
+        }
+
+        return Optional.empty();
     }
 
     public static String userInput(String question){
