6d9d1049f42d9633a11ff8fb17261def3b2da69f
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-03-29 21:18:03 -0400

    Created the course class which hold number of student and how many can be in their

--- src/main/java/org/example/Course/Course.java
+++ src/main/java/org/example/Course/Course.java
@@ -1,4 +1,47 @@
 package org.example.Course;
 
 public class Course {
+    String uuid;
+    String name;
+    final int maxNumberOfSeats = 50;
+    final double cost = 600;
+
+    public Course(String uuid, String name) {
+        this.uuid = uuid;
+        this.name = name;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxNumberOfSeats() {
+        return maxNumberOfSeats;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "uuid='" + uuid + '\'' +
+                ", name='" + name + '\'' +
+                ", maxNumberOfSeats=" + maxNumberOfSeats +
+                ", cost=" + cost +
+                '}';
+    }
 }
