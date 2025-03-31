ad9e4f107ea89a26953e92798b6dd603119f11d4
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-03-30 23:16:20 -0400

    Create the course table and establish the sql connection

---
+++ src/main/java/org/example/Course/Classes/MathClasses.java
@@ -0,0 +1,14 @@
+package org.example.Course.Classes;
+
+import org.example.Course.Course;
+
+public class Math extends Course {
+
+    public Math(String name, String subject) {
+        super(name, subject);
+    }
+
+    public Math(String name, String teacher, String subject) {
+        super(name, teacher, subject);
+    }
+}
--- src/main/java/org/example/Database/PostgresSQL.java
+++ src/main/java/org/example/Database/PostgresSQL.java
@@ -1,4 +1,52 @@
 package org.example.Database;
 
+import org.example.Course.Course;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PostgresSQL {
+
+    private Connection conn;
+    private Statement stmt;
+
+    public void createConnection(){
+        try{
+
+            String url = "jdbc:postgresql://localhost:5432/Student Management System";
+            String user = "postgres";
+            String password = "2001";
+
+             conn = DriverManager.getConnection(url, user, password);
+
+             stmt = conn.createStatement();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void createCourseTable() {
+        try{
+            String createCourseTableSQL = "CREATE TABLE IF NOT EXISTS Courses(UUID UUID NOT NULL  PRIMARY KEY, Name VARCHAR(255) NOT NULL, " +
+                    "Teacher VARCHAR(255), Subject VARCHAR(255) NOT NULL, MaxNumberOfSeats INT NOT NULL, Cost FLOAT NOT NULL)";
+
+            stmt.execute(createCourseTableSQL);
+
+            System.out.println("Course table created");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void closeConnection(){
+        try{
+            conn.close();
+            stmt.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
