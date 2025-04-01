68608244ed2a54d7cf1037d28aee3890e0d557d6
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-03-31 20:54:41 -0400

    Test changes and was able to add course to the table

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import org.example.Course.Course;
+import org.example.Course.CourseService;
+import org.example.Database.PostgresSQLDatabase;
 import org.example.Student.Student;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.Scanner;
@@ -12,109 +16,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Student student;
+        Course math_course = new Course("Calculus 1", "Math", "Kennedy Bravo");
+        Course science_course = new Course("Physics 1", "Science", "Michelle Ferguson");
+        Course history_course = new Course("Ancient Civilization", "History", "Rowan Zimmerman");
+        Course english_course = new Course("Reading & Research", "English", "Layla Stephenson");
 
-        ArrayList<Student> students = new ArrayList<>();
+        try{
+            PostgresSQLDatabase.createConnection();
 
-        Optional<Student> findStudent;
-        String uuid;
+            Connection db = new PostgresSQLDatabase().getConnection();
 
-        while(true){
-            String[] choices = {"1: Add Student","2: Get Student", "3: Update Student",
-            "4: Add Course"};
-            for(String choice: choices){
-                System.out.println(choice);
-            }
+            CourseService courseService = new CourseService(db);
 
-
-            // Get User input
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Please enter your choice: ");
-
-            int userChoice = Integer.parseInt(scanner.nextLine());
-
-            switch(userChoice){
-                case 1:
-                    String firstName = userInput("What's the firstname: ");
-                    String lastName = userInput("What's the lastname: ");
-                    double balance = Double.parseDouble(userInput("What's the balance: "));
-                    int age = Integer.parseInt(userInput("What's the age: "));
-
-                    student = new Student(firstName, lastName, age, balance);
-
-                    students.add(student);
-
-                    System.out.println(student);
-
-                    break;
-                case 2:
-                    uuid = userInput("What's the uuid: ");
-
-                    findStudent = findUserChoice(uuid, students);
-
-                    if(findStudent.isPresent()){
-                        System.out.println(findStudent);
-                    }
-
-                    break;
-                case 3:
-                    uuid = userInput("What's the uuid: ");
-                    findStudent = findUserChoice(uuid, students);
-
-                    if(findStudent.isEmpty()){
-                        break;
-                    }
-
-                    String updateFirstName = userInput("What's the firstname: ");
-                    String UpdateLastName = userInput("What's the lastname: ");
-                    double updateBalance = Double.parseDouble(userInput("What's the balance: "));
-                    int updateAge = Integer.parseInt(userInput("What's the age: "));
-
-                    findStudent.get().setFirstName(updateFirstName);
-                    findStudent.get().setLastName(UpdateLastName);
-                    findStudent.get().setBalance(updateBalance);
-                    findStudent.get().setAge(updateAge);
-
-                    System.out.println(findStudent);
-
-                    break;
-
-                case 4:
-                    uuid = userInput("What's the uuid: ");
-                    findStudent = findUserChoice(uuid, students);
-                    if(findStudent.isEmpty()){
-                        break;
-                    }
-
-                    addCourse(findStudent.get());
+            // Create the Course Table
+            courseService.createCoursesTable();
 
 
-            }
+            // Adding the Course to the table
+            courseService.addCourseToTable(math_course);
+            courseService.addCourseToTable(science_course);
+            courseService.addCourseToTable(history_course);
+            courseService.addCourseToTable(english_course);
 
+            PostgresSQLDatabase.close(db);
+
+        }catch(SQLException e){
+            e.printStackTrace();
         }
-
-    }
-
-    public static void addCourse(Student student){
-        int i = 0;
-        // Create Different Course
-        Course mathCourse = new Course("Math");
-        Course scienceCourse = new Course("Science");
-        Course englishCourse = new Course("English");
-        Course gymCourse = new Course("Gym");
-
-        Course[] courses = {mathCourse, scienceCourse, englishCourse, gymCourse};
-
-
-        for(Course course: courses){
-            System.out.println(i + ". " + course.getName());
-            i++;
-        }
-
-        int courseChoice = Integer.parseInt(userInput("Please enter your choice: "));
-
-
-        student.addCourse(courses[courseChoice]);
 
     }
 
