0fa8f168b3622f173089e7574cb98c7038291f8e
DevMorningCoffee <132598665+DEVMorningCoffee@users.noreply.github.com>
2025-04-05 23:27:10 -0400

    Added more exceptions to the program

--- src/main/java/org/example/Input/InputHelper.java
+++ src/main/java/org/example/Input/InputHelper.java
@@ -1,5 +1,7 @@
 package org.example.Input;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class InputHelper {
@@ -18,9 +20,10 @@ public class InputHelper {
                 System.out.print(prompt);
                 int value = Integer.parseInt(scanner.nextLine().trim());
                 if (value <= 0) throw new NumberFormatException("Value must be positive.");
+                if(value >= 100) throw new NumberFormatException("Value must be less than 100.");
                 return value;
             } catch (NumberFormatException e) {
-                System.out.println("❌ Invalid input: " + e.getMessage());
+                System.out.println("Invalid input: " + e.getMessage());
             }
         }
     }
@@ -29,12 +32,21 @@ public class InputHelper {
         while (true) {
             try {
                 System.out.print(prompt);
-                double value = Double.parseDouble(scanner.nextLine().trim());
+                double value = round(Double.parseDouble(scanner.nextLine().trim()),2);
                 if (value < 0) throw new NumberFormatException("Value must be non-negative.");
+                if(value >= 1e8) throw new NumberFormatException("Slow down millionaire you are too rich");
                 return value;
             } catch (NumberFormatException e) {
-                System.out.println("❌ Invalid input: " + e.getMessage());
+                System.out.println("Invalid input: " + e.getMessage());
             }
         }
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
